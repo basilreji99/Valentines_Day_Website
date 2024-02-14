5270da07ac96f47d4cc57099a6015602c4bedfad
Basil Reji <basil@Basil_Reji>
2024-02-13 23:12:26 -0500

    Changes the path in vite.config.ts file and package.json file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Valentines_Day_Website/",
+  base: "/Valentines_Day_Website/",
   plugins: [react()],
 });
